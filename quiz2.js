ebc8d5e6b1e0d831abb7ce58ee7e1ca67e8d6698
Nolan Lewis <nlewis5525@gmail.com>
2016-11-14 23:37:18 -0500

    made changes to quiz2

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,5 @@
-consle.log('welocme to quiz town, population...YOU!')
+alert('Welcome to quiz town, population...YOU!');
 
-@('.dynamic').hmtl('<div p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeIn('slo')
+$(document).ready(function(){
+$(".dynamic").append('<div><p>HI THERE, I\'M FROM JAVASCRIPT</p></div>').fadeOut("slow");
+});
